f7be57cbb96bc5d900bf78ba723dad88ea19f611
JC Villegas <juan.villegas@formstack.com>
2019-10-06 11:48:44 -0500

    Create view for create documents

--- views/documents/create.php
+++ views/documents/create.php
@@ -1,11 +1,15 @@
 <?php
 
+use app\models\Documents;
 use yii\helpers\Html;
+use yii\web\View;
+use yii\widgets\ActiveForm;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Documents */
 
-$this->title = 'Create Documents';
+$this->title = 'Create Document';
 $this->params['breadcrumbs'][] = ['label' => 'Documents', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -13,8 +17,61 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <div class="documents-form">
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'></script>
+        <script>
+            $(document).ready(function() {
+                let i = 1;
+                $('#add').click(function() {
+                    i++;
+                    $('#dynamic_field').append('' +
+                        '<tr id = "row'+i+'"> ' +
+                        '<td>' +
+                        '<label class="control-label">Key&nbsp; </label>' +
+                        '<input type=\'text\' name=\'key[]\'   required="true"> ' +
+                        '</td>' +
+                        '<td>' +
+                        '<label class="control-label">Value&nbsp; </label>' +
+                        '<input type=\'text\' name=\'value[]\' required="true">' +
+                        '<button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button>' +
+                        '</td>' +
+                        '</tr>'+
+                        '');
+                    $("#keyValueCounter").val(i);
+                });
+                $(document).on('click', '.btn_remove', function(){
+                    let buttonId = $(this).attr("id");
+                    i--;
+                    $('#row' + buttonId +'').remove();
+                    $("#keyValueCounter").val(i);
+                });
+            });
+
+        </script>
+        <div class="form-group field-documents-key_values required">
+
+            <table id='dynamic_field'>
+                <tr>
+                    <td> <label  class="control-label" for="documents-created">Key  </label> <input type='text' name='key[]'   required="true"> </td>
+                    <td> <label  class="control-label" for="documents-created">Value</label> <input type='text' name='value[]' required="true"> </td>
+                    <td> <button type='button' name='add' class="btn btn-success" id='add'>Add More</button>
+                </tr>
+            </table>
+        </div>
+        <input type='hidden' id="keyValueCounter" name="keyValueCounter">
+        <div class="form-group">
+            <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+    </div>
 
 </div>
+
+
+
