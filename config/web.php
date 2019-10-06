87182d35bf1736f340f8dde111d20631a6f8e6c7
JC Villegas <juan.villegas@formstack.com>
2019-10-06 12:11:46 -0500

    Fix  navigation and remove debugger

--- config/web.php
+++ config/web.php
@@ -4,6 +4,7 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
+    'name'=>'CRUD DEV Project',
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
@@ -57,7 +58,7 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
+    //$config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
