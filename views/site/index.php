be892fa5f38aef1ad55009b7ba1cd74cc925164a
JC Villegas <juan.villegas@formstack.com>
2019-10-06 21:46:57 -0500

    Refresh index view

--- views/site/index.php
+++ views/site/index.php
@@ -9,8 +9,22 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>CRUD DEV Project</h1>
 
-        <p class="lead">Generate Documents with key-value pairs. </p>
 
-        <p><a class="btn btn-lg btn-success" href="documents/create">Get started with it.</a></p>
+        <p><a class="btn btn-default" target="_blank" href="https://github.com/JCVillegas/juniorDEVProject">Github repository</a></p>
+
+
+
+        <div class="body-content">
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <p><a class="btn btn-lg btn-primary" href="/documents/create">Create a new document</a></p>
+                </div>
+                <div class="col-lg-6">
+                    <p><a class="btn btn-lg btn-primary" href="/documents/index">Read & Search a document</a></p>
+                </div>
+
+            </div>
+        </div>
     </div>
 </div>
