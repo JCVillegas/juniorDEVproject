6d14f565e914125eef8a01dcea2a59798509b12d
JC Villegas <juan.villegas@formstack.com>
2019-10-06 21:06:33 -0500

    Add dev envrionment token

--- config/params.php
+++ config/params.php
@@ -4,4 +4,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'dropboxToken' => 'o3YcmXH5SfsAAAAAAAALdCPLhiDc1yJGFmceFm9CD16pQ-Z3WPtJEJrxqi6-oXYw',
+
 ];
