852e88083ab8407253e137de0bf0854add911e2e
JC Villegas <juan.villegas@formstack.com>
2019-10-07 20:50:03 -0500

    Allow doc route in config

--- config/web.php
+++ config/web.php
@@ -16,6 +16,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'kCBEG_fVOcWUzepFDqOoPYwsWRgsJxM5',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -49,6 +52,10 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                [
+                    'class'      => 'yii\rest\UrlRule',
+                    'controller' => 'doc',
+                ],
             ],
         ],
 
